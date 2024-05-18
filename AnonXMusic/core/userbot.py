b94781ee2205fe7923a0836bc33ef79190697987
Nonenandu <noneofnone44@gmail.com>
2024-05-18 22:00:41 +0530

    Update userbot.py
    Signed-off-by: Nonenandu <noneofnone44@gmail.com>

--- AnonXMusic/core/userbot.py
+++ AnonXMusic/core/userbot.py
@@ -51,8 +51,8 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("DevilsHeavenMF")
-                await self.one.join_chat("FallenAssociation")
+                await self.one.join_chat("Telugu_chat_friendship")
+                await self.one.join_chat("ChinnuNetwork")
             except:
                 pass
             assistants.append(1)
@@ -72,8 +72,8 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("DevilsHeavenMF")
-                await self.two.join_chat("FallenAssociation")
+                await self.two.join_chat("Telugu_chat_friendship")
+                await self.two.join_chat("ChinnuNetwork")
             except:
                 pass
             assistants.append(2)
